6ddfd0ab0b49186628cb5b6c08b7812c590f4bb2
Papura Tavi <tavi93@yahoo.com>
2024-10-29 17:50:48 +0200

    Rezolvare

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <stack>
+#include <map>
 
 using namespace std;
+
+void demo1() {
+   
+}
 
 int main()
 {
     ifstream inFile("input.txt");
+    stack<pair<string, string>> pacienti;
+    map<string, string> doctori;
+    vector<string> pac, cauza; 
+    string doctoriiii;
+
 
     int no_problems, no_doctors;
     string name, speciality;
@@ -17,7 +29,12 @@ int main()
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        // pacienti.push(make_pair(name, speciality));
+
+        pac.push_back(name);
+        cauza.push_back(speciality);
+
+        /*cout << name << ' ' << speciality << '\n';*/
     }
 
     inFile >> no_doctors;
@@ -26,8 +43,36 @@ int main()
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        // doctori[speciality] = name;
+
+        doctoriiii += speciality;
+        //cout << name << ' ' << speciality << '\n';
     }
+
+    /*while (!pacienti.empty()) {
+        string cauza = pacienti.top().second;
+        if (!doctori[cauza].empty()) {
+            cout << pacienti.top().first << " Acceptat\n";
+        }
+        else {
+            cout << pacienti.top().first << " Respins\n";
+        }
+        pacienti.pop();
+    }*/
+
+    for (auto it = 0; it < cauza.size(); ++it) {
+        
+        if (doctoriiii.find(cauza[it]) != string::npos) {
+            cout << pac[it] << " Acceptat\n";
+        }
+        else{
+            cout << pac[it] << " Respins\n";
+        }
+    }
+
+
+
+
 
     return 0;
 }
